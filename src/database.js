6c6ba8ae96f47ea0f6c68b42ec6d5d962d637574
bakuchi- <helveticaf0015se@gmail.com>
2020-01-02 18:08:24 +0900

    modified description of databse.js

--- src/database.js
+++ src/database.js
@@ -2,11 +2,13 @@ module.exports = {
   "1": {
     "name": "オカピー",
     "birthday": "10-18",
-    "uri": "QmdCy6viWpHP7t8GgJxDC6jPMo5uPC4P94uM7mK2pYLMNT"
+    "uri": "QmUS6hCoCsJxZsBwVWTBKHE7YawYoJUf6sU5bQNeGAkne7",
+    "description": "ゴーストタイプ　ノ　オカピー．ニラミツケルヨウナ　スルドイシセンデ　シャシンニウツル　マルデ，ホラー"
   },
   "2": {
     "name": "Nagi",
-    "birthday": "01-08"
+    "birthday": "01-08",
+    "uri": "QmdCy6viWpHP7t8GgJxDC6jPMo5uPC4P94uM7mK2pYLMNT"
   },
   "3": {
     "name": "Buttle McFuddle",
