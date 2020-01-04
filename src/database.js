b20c9ecff956e2ab68faf1b9088f2582cfebf56f
bakuchi- <helveticaf0015se@gmail.com>
2020-01-04 17:51:26 +0900

    modified uri => undifined or GITS

--- src/database.js
+++ src/database.js
@@ -2,9 +2,9 @@ module.exports = {
   "1": {
     "name": "ラリピー",
     "birthday": "10-18",
-    // "uri": "QmQZuMujtvKd6A3dZS3M78a7F4eZoiNYdEdyY6ULFssanx",// Ghost In The Shell
-    // "uri": "https://pbs.twimg.com/media/Dmsl3w7VAAAHKvI.jpg:large",
-    "uri": "QmUS6hCoCsJxZsBwVWTBKHE7YawYoJUf6sU5bQNeGAkne7",
+    "uri": "QmQZuMujtvKd6A3dZS3M78a7F4eZoiNYdEdyY6ULFssanx",// Ghost In The Shell
+    // "uri": "https://pbs.twimg.com/media/Dmsl3w7VAAAHKvI.jpg:large", 
+    // "uri": "QmUS6hCoCsJxZsBwVWTBKHE7YawYoJUf6sU5bQNeGAkne7", //ラリぴ
     "description": "ラリちゃった",
     "type": "らり　タイプ",
     "attributes": [
@@ -20,7 +20,7 @@ module.exports = {
   "2": {
     "name": "ラリピーの眼",
     "birthday": "01-08",
-    "uri": "QmXn2v8s2juoe55Z17UxS4ti8tEkhy9XxN9uts1qhYXGHA",
+    // "uri": "QmXn2v8s2juoe55Z17UxS4ti8tEkhy9XxN9uts1qhYXGHA",
     "description": "目,Eye",
     "type": "鋭いまなざし",
     "attributes": [
@@ -37,7 +37,7 @@ module.exports = {
     "name": "ラリラリのラリピー",
     "birthday": "01-22",
     // "uri": "QmQZuMujtvKd6A3dZS3M78a7F4eZoiNYdEdyY6ULFssanx",// GHOST IN THE SHELL
-    "uri": "QmZmxQuHNssf7C7Fer2KDT4iT6L5Effh2NuANNiWquRUKR",
+    // "uri": "QmZmxQuHNssf7C7Fer2KDT4iT6L5Effh2NuANNiWquRUKR",
     "description": "ラリちゃった",
     "type": "ラリ　タイプ",
     "attributes": [
